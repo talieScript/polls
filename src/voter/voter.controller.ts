cc2907de3f19d735d6f96517d87288a559b2cbe7
Taliesin <taliesin.bowes@hotmail.co.uk>
2020-12-29 22:27:26 +0000

    get answers for user with email

--- src/voter/voter.controller.ts
+++ src/voter/voter.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { promises } from 'dns';
+import { Controller, Get, Param, Query, HttpStatus, HttpException, } from '@nestjs/common';
 import { VoterService } from './voter.service';
 
 @Controller('voter')
 export class VoterController {
   constructor(private readonly voterService: VoterService) {}
 
-  @Get('/answers/:voterId/:pollId')
-  async getAnswersForPoll(@Param('pollId') pollId: string, @Param('voterId') voterId: string): Promise<string[]> {
-    return await this.voterService.getAnswersForPoll({voterId, pollId})
+  @Get('/answers/:pollId')
+  async getAnswersForPoll(@Param('pollId') pollId: string, @Query('email') email: string,): Promise<string[]> {
+    if (!email) {
+      throw new HttpException({
+        status: HttpStatus.NOT_ACCEPTABLE,
+        error: 'Must provide email'
+      }, 406);
+    }
+    const voter = await this.voterService.getVoter({
+      where: {
+        email
+      }
+    })
+    if (!voter) {
+      return []
+    }
+    return await this.voterService.getAnswersForPoll({voterId: voter?.id, pollId})
   }
 }
