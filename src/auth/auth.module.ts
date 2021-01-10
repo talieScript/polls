d48a31627929a7de6681ce2e7e4556eb14522f78
Taliesin <taliesin.bowes@hotmail.co.uk>
2021-01-10 13:44:45 +0000

    Added authenticate user service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { VoterModule } from '../voter/voter.module';
+
 
 @Module({
+  imports: [VoterModule],
   providers: [AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { VoterService } from '../voter/voter.service';
+import { compare } from '../utils/passwordHashing'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private voterService: VoterService) {}
+
+  async authenticateVoter(email: string, pass: string): Promise<any> {
+    const voter = await this.voterService.getVoter({
+      where: { email }
+    })
+
+
+    
+    if(voter && compare(pass, voter.password )) {
+      return voter
+    }
+
+    return null
+  }
+}
