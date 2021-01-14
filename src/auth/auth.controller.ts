96b5eebab68461547860a3edcd3aaba92af55c8e
Taliesin <taliesin.bowes@hotmail.co.uk>
2021-01-14 21:10:05 +0000

    finished implementing discord authentocation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, Req, UseGuards, Request, Post, Body, Param } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
+import { VoterService } from '../voter/voter.service'
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 
 @Controller('auth')
 export class AuthController {
 
-    constructor( private readonly authService: AuthService) {}
+    constructor( private readonly authService: AuthService, private readonly voterService: VoterService) {}
 
     @Post('/sign-up')
     async signUp(@Body() SignUpDto: SignUpDto) {
@@ -37,5 +39,24 @@ export class AuthController {
     async getUserFromDiscord(@Param('code') code: string) {
 
       return await this.authService.getDiscordUserAndSignIn(code);
+    }
+
+    @Get('/user')
+    @UseGuards(JwtAuthGuard)
+    async getUser(@Request() req) {
+      const user = await this.voterService.getVoter({
+        where: {
+          id: req.user.userId
+        },
+        select: {
+          picture: true,
+          email: true,
+          name: true,
+          id: true
+        }
+      })
+      return {
+        user
+      }
     }
 }
